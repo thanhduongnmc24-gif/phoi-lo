0d527f8eb617057856a3fdd012fd23a0e51212ad
thanhduongnmc24-gif <thanhduongnmc24@gmail.com>
2026-04-18 05:09:14 +0000

    Cập nhật giao diện menu chuyên nghiệp

--- PhoiLo/MainWindow.xaml.cs
+++ PhoiLo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using PhoiLo.UserControls;
 using PhoiLo.Windows;
 using System.Data;
@@ -17,7 +18,58 @@ namespace PhoiLo
         {
             InitializeComponent();
             MainContentArea.Content = _sheetControl;
+            UpdateMenuHighlight(BtnMenuPulpit); // Mặc định cam cho Pulpit 1
             LoadStaffToHeader();
+        }
+
+        // [Suy luận] Hàm chuyên trách việc đổi màu nút bấm danh mục
+        private void UpdateMenuHighlight(Button activeBtn)
+        {
+            // Reset tất cả về màu trắng
+            BtnMenuPulpit.Foreground = Brushes.White;
+            BtnMenuHot.Foreground = Brushes.White;
+            BtnMenuKcs.Foreground = Brushes.White;
+            BtnMenuSetting.Foreground = Brushes.White;
+
+            BtnMenuPulpit.FontWeight = FontWeights.Normal;
+            BtnMenuHot.FontWeight = FontWeights.Normal;
+            BtnMenuKcs.FontWeight = FontWeights.Normal;
+
+            // Nhuộm cam nút đang chọn
+            activeBtn.Foreground = (Brush)new BrushConverter().ConvertFromString("#e67e22");
+            activeBtn.FontWeight = FontWeights.Bold;
+        }
+
+        private void BtnLoadSheet_Click(object sender, RoutedEventArgs e)
+        {
+            _sheetControl.LoadDataFromGoogle(); 
+            MainContentArea.Content = _sheetControl;
+            UpdateMenuHighlight(BtnMenuPulpit);
+        }
+
+        private void BtnHotBillet_Click(object sender, RoutedEventArgs e)
+        {
+            var dv = _sheetControl.MainDataGrid.ItemsSource as DataView;
+            if (dv != null && dv.Table != null) _hotControl.SetData(dv.Table);
+            MainContentArea.Content = _hotControl;
+            UpdateMenuHighlight(BtnMenuHot);
+        }
+
+        private void BtnLoadKcs_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = _kcsControl;
+            UpdateMenuHighlight(BtnMenuKcs);
+        }
+
+        private void BtnOpenSetting_Click(object sender, RoutedEventArgs e) {
+            UpdateMenuHighlight(BtnMenuSetting);
+            SettingWindow setWin = new SettingWindow { Owner = this };
+            setWin.ShowDialog();
+            LoadStaffToHeader();
+            // Sau khi đóng setting, quay lại highlight cái tab đang hiển thị thực tế
+            if (MainContentArea.Content == _sheetControl) UpdateMenuHighlight(BtnMenuPulpit);
+            else if (MainContentArea.Content == _hotControl) UpdateMenuHighlight(BtnMenuHot);
+            else if (MainContentArea.Content == _kcsControl) UpdateMenuHighlight(BtnMenuKcs);
         }
 
         private void CbGlobalKip_SelectionChanged(object sender, SelectionChangedEventArgs e) => LoadStaffToHeader();
@@ -34,34 +86,6 @@ namespace PhoiLo
             CbGlobalVanHanh.ItemsSource = staff.Where(s => s.Kip.ToUpper() == group && s.ChucVu.Contains("Vận hành")).ToList();
             if (CbGlobalToTruong.Items.Count > 0) CbGlobalToTruong.SelectedIndex = 0;
             if (CbGlobalVanHanh.Items.Count > 0) CbGlobalVanHanh.SelectedIndex = 0;
-        }
-
-        private void BtnLoadSheet_Click(object sender, RoutedEventArgs e)
-        {
-            _sheetControl.LoadDataFromGoogle(); 
-            MainContentArea.Content = _sheetControl;
-        }
-
-        private void BtnHotBillet_Click(object sender, RoutedEventArgs e)
-        {
-            // [Suy luận] Lấy bảng dữ liệu từ SheetControl truyền sang HotControl
-            var dv = _sheetControl.MainDataGrid.ItemsSource as DataView;
-            
-            // Thêm điều kiện dv.Table != null để C# yên tâm 100% không bị rỗng
-            if (dv != null && dv.Table != null) 
-            {
-                _hotControl.SetData(dv.Table);
-            }
-            
-            MainContentArea.Content = _hotControl;
-        }
-
-        private void BtnLoadKcs_Click(object sender, RoutedEventArgs e) => MainContentArea.Content = _kcsControl;
-
-        private void BtnOpenSetting_Click(object sender, RoutedEventArgs e) {
-            SettingWindow setWin = new SettingWindow { Owner = this };
-            setWin.ShowDialog();
-            LoadStaffToHeader(); 
         }
     }
 }
